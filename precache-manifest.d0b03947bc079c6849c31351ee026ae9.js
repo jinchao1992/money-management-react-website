f26dd61d05715faffe5038406b4bc2f4192e7fca
jinchao1992 <jinchaophp@126.com>
2020-08-19 23:28:37 +0800

    init

---
+++ precache-manifest.d0b03947bc079c6849c31351ee026ae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9760c00132e844554b40684f01f38e38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65c7c20b7191f611aa26",
+    "url": "/static/css/main.d9b513bb.chunk.css"
+  },
+  {
+    "revision": "31e4bb1e6f4ff0c5b1f3",
+    "url": "/static/js/2.96f7114d.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/static/js/2.96f7114d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c7c20b7191f611aa26",
+    "url": "/static/js/main.63d56328.chunk.js"
+  },
+  {
+    "revision": "6207bd4ad42cfb8b7681",
+    "url": "/static/js/runtime-main.2365fa18.js"
+  }
+]);
